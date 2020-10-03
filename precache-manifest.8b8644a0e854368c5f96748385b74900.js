e5e2d7901c87aa8d567d17114a519db3b904126c
FrontRowWithJ <adebusum@tcd.ie>
2020-10-03 19:58:40 +0100

    Updates

---
+++ precache-manifest.8b8644a0e854368c5f96748385b74900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6416ce41053221087245bb2068834edc",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a0632c160b00faf3675c",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/Portfolio/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0632c160b00faf3675c",
+    "url": "/Portfolio/static/js/main.1175e5fe.chunk.js"
+  },
+  {
+    "revision": "5e606d1b3306133532c3",
+    "url": "/Portfolio/static/js/runtime-main.a8c0044a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
