f75156fd8c62833e3b70faa0a22ec1e74b8baf69
FrontRowWithj <adebusum@tcd.ie>
2021-09-18 17:34:41 +0100

    Linear gradient is now applied to the SVG textbox to make it look prettier

--- src/ProjectTextBox.jsx
+++ src/ProjectTextBox.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const ProjectTextBox = (props) => {
   const from =
     "M 32 512 L 480 512 C 496 512 512 496 512 480 L 512 32 C 512 16 496.1235539725813 1.9845557534273355 480.2471079451626 3.969111506854671 L 31.752892054837368 60.030888493145326 C 15.876446027418684 62.01544424657266 0 80 0 96 L 0 480 C 0 496 16 512 32 512 Z";
@@ -9,12 +8,22 @@ const ProjectTextBox = (props) => {
     <svg
       id={props.id}
       className={props.className}
-      style={{ filter: props.filter }}
+      // style={{ filter: props.filter }}
       preserveAspectRatio="none"
       viewBox="0 0 512 512"
       onClick={() => props.animateSVG()}
     >
-      <path id="box" fill="#002642" d={props.isForward ? from : to}>
+      <defs>
+        <linearGradient id="MyGradient" gradientTransform="rotate(15)">
+          <stop offset="0%" stopColor="#2b2d42" />
+          <stop offset="100%" stopColor="#2f3e46" />
+        </linearGradient>
+      </defs>
+      <path
+        id="box"
+        style={{ fill: "url(#MyGradient)" }}
+        d={props.isForward ? from : to}
+      >
         <animate
           id={props.animID}
           fill="freeze"
