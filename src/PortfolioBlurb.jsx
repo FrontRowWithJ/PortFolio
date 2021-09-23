65562fc56c46d3678ebe4d35b805451fd1a8be31
FrontRowWithj <adebusum@tcd.ie>
2021-09-23 23:55:01 +0100

    Restyled the portfolio blurb to incorporate the background in its design

--- src/PortfolioBlurb.jsx
+++ src/PortfolioBlurb.jsx
@@ -1,38 +1,31 @@
 import React from "react";
-import LinkedIn from "./LinkedIn";
-import Github from "./Github";
 import headshot from "./images/empty-headshot.jpg";
 import { setFactor, BACKWARD } from "./newParticle";
 import "./tailwind.output.css";
-import "./style/index.css";
+import "./style/portfolio-blurb.css";
+import SocialLink from "./SocialLink";
 
 const PortfolioBlurb = () => {
   return (
-    <div id="blurb-bg" className="w-64 lg:w-1/4">
-      <div id="blurb">
-        <div id="prof-container">
-          <img id="profile-photo" src={headshot} alt="Michael Adebusuyi" />
+    <div id="blurb-container">
+      <div>
+        <div>
+          Hey! I'm Michael. Do you like what you see? Then let's get in
+          <a href="mailto:michael.adebu@gmail.com">&nbsp;touch!</a>
+          <SocialLink icon="linkedin" fill="white" className="social-link" />
         </div>
-        <div id="desc">
-          <div>
-            Hey! I'm Michael. Do you like what you see? Then let's get in
-            <a className="text-white" href="mailto:michael.adebu@gmail.com">
-              &nbsp;touch!
-            </a>
-            &nbsp; If not, I'll leave you with something
-            <a
-              href="#/404"
-              className="text-white"
-              onClick={() => setFactor(BACKWARD)}
-            >
-              &nbsp;better!
-            </a>
-          </div>
+      </div>
+      <div>
+        <img src={headshot} alt="Michael Adebusuyi" />
+      </div>
+      <div>
+        <div>
+          If not, I'll leave you with something
+          <a href="#/404" onClick={() => setFactor(BACKWARD)}>
+            &nbsp;better!
+          </a>
         </div>
-        <div id="links">
-          <LinkedIn fill="white" className="linkedin-svg" />
-          <Github fill="white" className="github-svg" />
-        </div>
+        <SocialLink icon="github" fill="white" className="social-link" />
       </div>
     </div>
   );
