212b8377aebea74e2e8b4d7535d25be3c1cb378c
FrontRowWithj <adebusum@tcd.ie>
2021-09-23 23:47:08 +0100

    Added cleanup function to avoid memory leak and appending the same event too many times

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import PortfolioBlurb from "./PortfolioBlurb";
 import ProjectDisplay from "./ProjectDisplay";
-import { PROJECT_LIST, pageState } from "./PageState";
+import { PROJECT_LIST, pageState } from "./pageState";
 
 const Home = () => {
   const [animateForward, setAnimateForward] = useState(undefined);
   const [toggle, setToggle] = useState(true);
 
-  useEffect(() => {
-    document.body.addEventListener("keyup", (event) => {
+  const handleKeyUp = useCallback(
+    (event) => {
       if (
         event.key === "Escape" &&
         pageState.state === PROJECT_LIST &&
@@ -17,31 +17,33 @@ const Home = () => {
         setAnimateForward(false);
         setToggle(true);
       }
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    },
+    [setAnimateForward, setToggle, toggle]
+  );
+  useEffect(() => {
+    document.body.addEventListener("keyup", handleKeyUp);
+    return () => document.addEventListener("keyup", handleKeyUp);
+  }, [handleKeyUp]);
 
   return (
-    <>
-      <main
-        onClick={(e) => {
-          const nodeName = e.target.nodeName;
-          if ((nodeName === "MAIN" || nodeName === "CANVAS") && !toggle) {
-            setAnimateForward(false);
-            setToggle(true);
-          }
-        }}
-        className="h-full w-full flex flex-col items-center relative"
-      >
-        <PortfolioBlurb />
-        <ProjectDisplay
-          animateForward={animateForward}
-          setAnimateForward={setAnimateForward}
-          toggle={toggle}
-          setToggle={setToggle}
-        />
-      </main>
-    </>
+    <main
+      onClick={(e) => {
+        const nodeName = e.target.nodeName;
+        if ((nodeName === "MAIN" || nodeName === "CANVAS") && !toggle) {
+          setAnimateForward(false);
+          setToggle(true);
+        }
+      }}
+      className="h-full w-full flex flex-col items-center relative"
+    >
+      <PortfolioBlurb />
+      <ProjectDisplay
+        animateForward={animateForward}
+        setAnimateForward={setAnimateForward}
+        toggle={toggle}
+        setToggle={setToggle}
+      />
+    </main>
   );
 };
 export default Home;
