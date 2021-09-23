857e7be2d8ddfd1edac9632d326c6b2e7ef3b3f5
FrontRowWithj <adebusum@tcd.ie>
2021-09-24 00:05:24 +0100

    Moved imported functions locally. Fixed issue where the first value of the zIndex array would always be 1. Fixed issue where website would break when the viewport changed size.

--- src/ProjectDisplay.jsx
+++ src/ProjectDisplay.jsx
@@ -1,14 +1,55 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import NavButton from "./ProjectNavigate";
 import ProjectBlurb from "./ProjectBlurb";
 import ProjectButton from "./ProjectButton";
 import { times } from "lodash";
 import projects from "./projectData";
+import { PROJECT_DESC, pageState } from "./pageState";
+
+const getParentRect = () => {
+  return document
+    .getElementsByClassName("project-blurb")[0]
+    .getBoundingClientRect();
+};
+
+const positionSVG = (svg) => {
+  document.getElementsByClassName(
+    "project-textbox"
+  )[0].style.transitionDuration = "0s";
+  const rect = getParentRect();
+  svg.style.top = svg.style.left = 0;
+  svg.style.transform = `translate(${-rect.left}px, ${-rect.top}px)`;
+};
+
+const positionImage = (img, offset) => {
+  const parentRect = getParentRect();
+  const rect = img.getBoundingClientRect();
+  img.style.left = img.style.top = 0;
+  const left = (window.innerWidth - rect.width) / 2 - parentRect.left;
+  const top = parentRect.top - offset;
+  img.style.transform = `translate(${left}px, ${-top}px)`;
+};
 
 const ProjectDisplay = (props) => {
-  const [zIndices, setIndices] = useState(times(projects.length, () => 0));
-  zIndices[0] = 1;
+  const [zIndices, setIndices] = useState([1, 0]);
   const blurbRefs = useRef(times(projects.length, () => React.createRef()));
+  const handleResize = useCallback(() => {
+    const i = zIndices.indexOf(1);
+    const [svg, img] = ["svg", "img"].map((s) =>
+      document.getElementById(`${s}-${i}`)
+    );
+    const offset = 15;
+    if (pageState.state === PROJECT_DESC) {
+      positionSVG(svg);
+      positionImage(img, offset);
+    }
+  }, [zIndices]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
+
   return (
     <div className="flex justify-center items-center">
       {projects.map((obj, i) => {
@@ -26,7 +67,6 @@ const ProjectDisplay = (props) => {
             projectImage={obj.projectImage}
             projectLink={obj.projectLink}
             zIndex={zIndices[i]}
-            textboxID={obj.textboxID}
             timeline={obj.timeline}
             about={obj.about}
             techlist={obj.techlist}
