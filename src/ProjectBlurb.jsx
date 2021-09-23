a44211b7ec7c3ac17c5f7b088ebb82d9c0a888a9
FrontRowWithj <adebusum@tcd.ie>
2021-09-24 00:00:01 +0100

    Moved exported functions to locally because they are only used here. Some code minification. Added restyling that would reset the styles to their original values because changing the viewport size causes these values to change. Textbox id now has the same naming convention as all the other elements

--- src/ProjectBlurb.jsx
+++ src/ProjectBlurb.jsx
@@ -1,11 +1,45 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
-import LinkedIn from "./LinkedIn";
-import Github from "./Github";
+import SocialLink from "./SocialLink";
 import Textbox from "./ProjectTextBox";
 import "./style/proj-blurb.css";
 import Arrow from "./Arrow";
 import ImageView from "./ImageView";
-import { PROJECT_DESC, PROJECT_LIST, pageState } from "./PageState";
+import { PROJECT_DESC, PROJECT_LIST, pageState } from "./pageState";
+
+const transformSVG = (svg) => {
+  const rect = svg.getBoundingClientRect();
+  const translateX = rect.left + rect.width / 2 - window.innerWidth / 2;
+  const translateY = window.innerHeight - rect.bottom;
+  svg.style.transform = `translate(${-translateX}px, ${translateY}px)`;
+};
+
+const transformImage = (img, offset) => {
+  const rect = img.getBoundingClientRect();
+  const translateX = (window.innerWidth - rect.right - rect.x) / 2;
+  const translateY = rect.top - offset;
+  img.style.transform = `translate(${translateX}px, ${-translateY}px)`;
+};
+
+const transformLinkedin = (linkedin, offset) => {
+  const rect = linkedin.getBoundingClientRect();
+  const translateX = rect.x - offset;
+  const translateY = window.innerHeight - rect.bottom - offset;
+  linkedin.style.transform = `translate(${-translateX}px, ${translateY}px)`;
+};
+
+const transformGithub = (github, offset) => {
+  const rect = github.getBoundingClientRect();
+  const translateX = (window.innerWidth - rect.right - rect.x) / 2;
+  const translateY = window.innerHeight - rect.bottom - offset;
+  github.style.transform = `translate(${translateX}px, ${translateY}px)`;
+};
+
+const transformViewProject = (viewProject, offset) => {
+  const rect = viewProject.getBoundingClientRect();
+  const translateX = window.innerWidth - rect.right - offset;
+  const translateY = window.innerHeight - rect.bottom - offset;
+  viewProject.style.transform = `translate(${translateX}px, ${translateY}px)`;
+};
 
 const ProjectBlurb = (props) => {
   const filter = `drop-shadow(-20px -20px 10px #201b28) drop-shadow(20px 20px 10px #0e0d12)`;
@@ -17,36 +51,35 @@ const ProjectBlurb = (props) => {
   const animateSVG = useCallback(() => {
     if (props.zIndex === 1) {
       const idx = props.index;
-      const animate = document.getElementById("anim-" + idx);
-      const svg = document.getElementById(props.textboxID);
-      const img = document.getElementById("img-" + idx);
-      const linkedin = document.getElementById("linkedin-" + idx);
-      const github = document.getElementById("github-" + idx);
-      const viewProject = document.getElementById("viewProject-" + idx);
-      const text = document.getElementById("text-" + idx);
-      const about = document.getElementById("about-" + idx);
+      const ids = ["anim", "svg", "img", "li", "gi", "vp", "txt", "abt"];
+      const elems = ids.map((id) => document.getElementById(`${id}-${idx}`));
+      const [anim, svg, img, li, gi, vp, txt, abt] = elems;
       setDirection(isFirst ? true : !isForward);
-      animate.beginElement();
+      anim.beginElement();
       svg.classList.toggle("animate");
       const offset = 15;
-      const refs = [svg, img, linkedin, github, viewProject];
+      const refs = [svg, img, li, gi, vp];
       if (value) {
         transformSVG(svg);
         transformImage(img, offset);
-        transformLinkedin(linkedin, offset);
-        transformGithub(github, offset);
-        transformViewProject(viewProject, offset);
-      } else refs.forEach((ref) => (ref.style.transform = ""));
-      [text, linkedin, github, viewProject, about].forEach((ref) =>
-        toggleNode(value, ref)
-      );
+        transformLinkedin(li, offset);
+        transformGithub(gi, offset);
+        transformViewProject(vp, offset);
+      } else {
+        document.getElementsByClassName(
+          "project-textbox"
+        )[0].style.transitionDuration = "1s";
+        svg.style.left = svg.style.top = "";
+        img.style.left = img.style.top = "";
+        refs.forEach((ref) => (ref.style.transform = ""));
+      }
+      [txt, li, gi, vp, abt].forEach((ref) => toggleNode(value, ref));
       toggleNode(value, svg, 0.9, "");
       toggleValue(!value);
       setIsFirst(false);
     }
   }, [
     props.zIndex,
-    props.textboxID,
     props.index,
     isFirst,
     isForward,
@@ -91,8 +124,8 @@ const ProjectBlurb = (props) => {
           </section>
           <section className="view-project-section absolute right-0 max-h-full grid">
             <a href={props.projectLink}>View Project</a>
-            <Github fill="none" stroke="white" />
-            <LinkedIn fill="none" stroke="white" />
+            <SocialLink icon="github" fill="none" stroke="white" />
+            <SocialLink icon="linkedin" fill="none" stroke="white" />
           </section>
         </nav>
         <article className="project-info-desc">
@@ -147,12 +180,12 @@ const ProjectBlurb = (props) => {
               fill={"#17141D"}
               filter={filter}
               animID={"anim-" + props.index}
-              id={props.textboxID}
+              id={"svg-" + props.index}
               animateSVG={animateSVG}
               className="project-textbox absolute bottom-0"
             />
             <div
-              id={"text-" + props.index}
+              id={"txt-" + props.index}
               className="project-text absolute bottom-0 project-desc text-white"
             >
               <div className="project-text-hover relative">
@@ -163,13 +196,13 @@ const ProjectBlurb = (props) => {
               </div>
             </div>
             <div
-              id={"about-" + props.index}
+              id={"abt-" + props.index}
               style={{ transition: "opacity .4s linear" }}
               className="about-button absolute"
               onClick={() => animateSVG()}
             >
               View More
-              <div className="view-more-hover absolute h-full top-0 uppercase bg-clip-text">
+              <div className="view-more-hover absolute h--full top-0 uppercase bg-clip-text">
                 View _animateSVGMore
               </div>
             </div>
@@ -181,18 +214,20 @@ const ProjectBlurb = (props) => {
             />
           </div>
           <div className="link-container absolute bottom-0 flex flex-row w-full items-center self-end">
-            <LinkedIn
-              id={"linkedin-" + props.index}
+            <SocialLink
+              icon="linkedin"
+              id={"li-" + props.index}
               linkClass="linkedin-link"
               fill="white"
             />
-            <Github
-              id={"github-" + props.index}
+            <SocialLink
+              icon="github"
+              id={"gi-" + props.index}
               fill="white"
               linkClass="github-link"
             />
             <a
-              id={"viewProject-" + props.index}
+              id={"vp-" + props.index}
               className="project-link rounded-full text-white flex justify-center"
               href={props.projectLink}
             >
@@ -220,41 +255,6 @@ const toggleNode = (value, elem, opacity = 0, zIndex = "-1") => {
       pageState.state = PROJECT_LIST;
     };
   }
-};
-
-const transformSVG = (svg) => {
-  const rect = svg.getBoundingClientRect();
-  const translateX = rect.left + rect.width / 2 - window.innerWidth / 2;
-  const translateY = window.innerHeight - rect.bottom;
-  svg.style.transform = `translate(${-translateX}px, ${translateY}px)`;
-};
-
-const transformImage = (img, offset) => {
-  const rect = img.getBoundingClientRect();
-  const translateX = (window.innerWidth - rect.right - rect.x) / 2;
-  const translateY = rect.top - offset;
-  img.style.transform = `translate(${translateX}px, ${-translateY}px)`;
-};
-
-const transformLinkedin = (linkedin, offset) => {
-  const rect = linkedin.getBoundingClientRect();
-  const translateX = rect.x - offset;
-  const translateY = window.innerHeight - rect.bottom - offset;
-  linkedin.style.transform = `translate(${-translateX}px, ${translateY}px)`;
-};
-
-const transformGithub = (github, offset) => {
-  const rect = github.getBoundingClientRect();
-  const translateX = (window.innerWidth - rect.right - rect.x) / 2;
-  const translateY = window.innerHeight - rect.bottom - offset;
-  github.style.transform = `translate(${translateX}px, ${translateY}px)`;
-};
-
-const transformViewProject = (viewProject, offset) => {
-  const rect = viewProject.getBoundingClientRect();
-  const translateX = window.innerWidth - rect.right - offset;
-  const translateY = window.innerHeight - rect.bottom - offset;
-  viewProject.style.transform = `translate(${translateX}px, ${translateY}px)`;
 };
 
 export default ProjectBlurb;
